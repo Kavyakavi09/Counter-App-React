659f20e09b48180d54751bd878ecad90731afdcd
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-05-06 23:46:42 +0530

    counter app finished

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './App.css';
+import Header from './components/header'
+
+function App() {
+
+  const [counter,setCounter]= useState(0);
+
+  function increase(){
+   setCounter(counter+1)
+  }
+
+  function decrease(){
+    setCounter(counter-1)  
+  }
+
+  return (
+  
+   <div>
+   <div className='counter-body'>
+   <Header />
+      <h2 className='App'> Counter:{counter}</h2> 
+    <div className='counter-div'>
+    <button onClick={increase} className="counter">Increase the counter</button>
+    <button onClick={decrease} className="counter">decrease the counter</button>
+    </div>
+   </div>
+    </div>
+   );
+}
+
+export default App;
