1fe00c895714fab977d39ec1fe1795db4f485d01
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-05-19 01:39:25 +0530

    changed transition

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 import Header from './components/header'
 
 function App() {
+  const [counter, setCounter] = useState(0)
 
-  const [counter,setCounter]= useState(0);
-
-  function increase(){
-   setCounter(counter+1)
+  function increase() {
+    setCounter(counter + 1)
   }
 
-  function decrease(){
-    setCounter(counter-1)  
+  function decrease() {
+    setCounter(counter - 1)
   }
 
   return (
-  
-   <div>
-   <div className='counter-body'>
-   <Header />
-      <h2 className='App'> Counter:{counter}</h2> 
-    <div className='counter-div'>
-    <button onClick={increase} className="counter">Increase the counter</button>
-    <button onClick={decrease} className="counter">decrease the counter</button>
+    <div>
+      <div className="counter-body">
+        <Header />
+        <h2 className="App"> Counter:{counter}</h2>
+        <div className="counter-div">
+          <button onClick={increase} className="counter">
+            Increase the counter
+          </button>
+          <button onClick={decrease} className="counter">
+            decrease the counter
+          </button>
+        </div>
+      </div>
     </div>
-   </div>
-    </div>
-   );
+  )
 }
 
-export default App;
+export default App
